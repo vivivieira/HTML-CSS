ffd108004f5718aa90e8c01620f65a52f04108ed
Viviane Vieira <viviivieiras@gmail.com>
2022-02-03 13:38:40 -0300

    atualizações

---
+++ JavaScript/aula12/ex001.js
@@ -0,0 +1,10 @@
+var idade = 12
+if (idade < 16) {
+    console.log(`Não vota`)
+} else if (idade < 18) {
+            console.log(`Voto opcional`)
+} else {
+    console.log(`Voto obrigatório`)
+}
+
+
